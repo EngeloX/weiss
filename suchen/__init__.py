7e5cc5959627d6981f9d5442431cd70328670d23
EngeloX <evilguroff@gmail.com>
2025-10-06 05:45:55 +0600

    restructured

---
+++ suchen/__init__.py
@@ -0,0 +1,5 @@
+from ._suchen import IntDistrib, FloatDistrib, LogDistrib, CatDistrib, sample_params, Searcher
+
+
+
+__all__ = ['IntDistrib', 'FloatDistrib', 'LogDistrib', 'CatDistrib', 'sample_params', 'Searcher']
